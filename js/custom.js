fb195a49235eebb5db5a8f68614184db23025c05
NiravBeladiya <144112628+NiravBeladiya@users.noreply.github.com>
2023-09-05 16:31:29 +0530

    done

---
+++ js/custom.js
@@ -0,0 +1,33 @@
+$(document).ready(function () {
+
+$('.toggle-btn').click(function(){
+	$("body").toggleClass('menu_opan');
+})
+
+$('.moving-company-list').hide();
+
+	$('.plus-icon').click(function(){
+		$('.moving-company-list').slideToggle('linear');
+})
+	
+	$('.Testimonial').owlCarousel({
+		    loop:true,
+		    margin:10,
+		    nav:true,
+		    dots:true,
+		    item:3,
+		    navText: ["<i class='fas fa-arrow-left'></i>","<i class='fas fa-arrow-right'></i>"],
+		    responsive:{
+		        0:{
+		            items:1
+		        },
+		        600:{
+		            items:3
+		        },
+		        1000:{
+		            items:3
+		        }
+		    }
+		})
+
+});
